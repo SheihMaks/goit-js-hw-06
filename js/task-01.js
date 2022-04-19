f8bd67ffbbe3f250717d564be8db11959bc57020
Maksym Padalka <sheihmaks@gmail.com>
2022-04-20 01:45:18 +0300

    Update task-01.js

--- js/task-01.js
+++ js/task-01.js
@@ -3,7 +3,6 @@ const listItemRef = document.querySelectorAll(".item");
 console.log(`Number of categories: ${listItemRef.length}`);
 
 for (const item of listItemRef) {
-  const subcatQuantityRef = item.querySelectorAll("li").length;
   console.log(`Category: ${item.firstElementChild.textContent}`);
-  console.log(`Elements: ${subcatQuantityRef}`);
+  console.log(`Elements: ${item.querySelectorAll("li").length}`);
 }
