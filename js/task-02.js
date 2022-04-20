becacfe3cc37fe71d7f0e5c2ef2d77d15a2338ec
Maksym Padalka <sheihmaks@gmail.com>
2022-04-21 01:49:38 +0300

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,18 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const ingredientsContainer = document.querySelector("#ingredients");
+
+const itemIngredients = ingredients.map((ingredient) => {
+  const itemEl = document.createElement("li");
+  itemEl.classList.add("item");
+  itemEl.textContent = ingredient;
+  return itemEl;
+});
+ingredientsContainer.append(...itemIngredients);
