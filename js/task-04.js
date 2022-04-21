d9084bd22e8a66a4d9d4a94cfecfec2dd5638008
Maksym Padalka <sheihmaks@gmail.com>
2022-04-22 02:03:17 +0300

    +

---
+++ js/task-04.js
@@ -0,0 +1,24 @@
+const counter = {
+  value: 0,
+  increment() {
+    this.value += 1;
+  },
+  decrement() {
+    this.value -= 1;
+  },
+};
+const refs = {
+  decrementBtnEl: document.querySelector('[data-action="decrement"]'),
+  valueOfCounterEl: document.querySelector("#value"),
+  incrementBtnEl: document.querySelector('[data-action="increment"]'),
+};
+console.log(refs);
+refs.decrementBtnEl.addEventListener("click", function () {
+  counter.decrement();
+  refs.valueOfCounterEl.textContent = counter.value;
+});
+
+refs.incrementBtnEl.addEventListener("click", function () {
+  counter.increment();
+  refs.valueOfCounterEl.textContent = counter.value;
+});
