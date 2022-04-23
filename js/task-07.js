63f9ec96a49d1a99340961bfe76116afa6faefab
Maksym Padalka <sheihmaks@gmail.com>
2022-04-23 18:55:25 +0300

    +

---
+++ js/task-07.js
@@ -0,0 +1,10 @@
+const refs = {
+  input: document.querySelector("#font-size-control"),
+  exampleFontSize: document.querySelector("#text"),
+};
+
+const onEventInput = (event) => {
+  refs.exampleFontSize.style.fontSize = `${event.currentTarget.value}px`;
+};
+
+refs.input.addEventListener("input", onEventInput);
