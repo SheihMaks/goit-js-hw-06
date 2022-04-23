049e5c7316ea1853e4b234f151cb22e80fef9c5b
Maksym Padalka <sheihmaks@gmail.com>
2022-04-23 16:28:03 +0300

    +

--- js/task-06.js
+++ js/task-06.js
@@ -1,16 +1,11 @@
-const refs = {
-  input: document.querySelector("#validation-input"),
-};
-console.dir(refs.input);
-refs.input.classList.add("#validation-input");
+const inputRef = document.querySelector("#validation-input");
+
+inputRef.classList.add("#validation-input");
 const onValidationValue = (event) => {
-  console.log(
-    Number(event.currentTarget.getAttribute("data-length")) ===
-      event.currentTarget.value.length
-  );
+  event.currentTarget.classList.remove("invalid");
   event.currentTarget.value.length ===
   Number(event.currentTarget.getAttribute("data-length"))
     ? event.currentTarget.classList.add("valid")
     : event.currentTarget.classList.add("invalid");
 };
-refs.input.addEventListener("blur", onValidationValue);
+inputRef.addEventListener("blur", onValidationValue);
